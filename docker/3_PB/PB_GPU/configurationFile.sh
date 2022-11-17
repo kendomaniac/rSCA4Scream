f405b153a185a2312f52696acde665b7bb37557a
Luca <lucastormreig@pop-os.localdomain>
2022-11-17 09:51:18 -0500

    yo

---
+++ docker/3_PB/PB_GPU/configurationFile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#type here all libraries that needs to be installed
+download pandas
+download numpy
+download matplotlib
+download scikit-learn
+download tensorflow-gpu
+download keras 
