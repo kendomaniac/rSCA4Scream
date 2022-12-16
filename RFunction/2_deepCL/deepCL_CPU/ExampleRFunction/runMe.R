d8d6dfaea729ed237abdc5db3b0d5ee46ee3f688
Luca <lucastormreig@pop-os.localdomain>
2022-12-16 16:02:39 -0500

    asd

---
+++ RFunction/2_deepCL/deepCL_CPU/ExampleRFunction/runMe.R
@@ -0,0 +1,11 @@
+source("runDocker.R")
+source("deepCL.R")
+scratch.folder=getwd()
+file=paste(scratch.folder,"setA.csv",sep="/")
+separator=","
+bias="kegg"
+permutation=40
+nEpochs=1000
+projectName="Test_CPU"
+nCluster=3
+deepClustering(group=c("docker"), scratch.folder=scratch.folder, file=file,separator=separator,nCluster=nCluster, bias=bias, finetune_iters=10, nEpochs=50,projectName=projectName,bN="NULL")
---
+++ RFunction/2_deepCL/deepCL_CPU/deepCL.R
@@ -0,0 +1,124 @@
+#' @title deepClustering
+#' @description The present function compress data using autoencoder partially connected
+#' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
+#' @param scratch.folder, a character string indicating the path of the scratch folder
+#' @param file, a character string indicating the path of the file, with file name and extension included
+#' @param finetune_iters, number of deep iteration
+#' @param nCluster, number of cluster in which the dataset is divided
+#' @param nEpochs, number of Epochs for neural network training
+#' @param projectName, might be different from the matrixname in order to perform different analysis on the same dataset
+#' @param separator, separator used in count file, e.g. '\\t', ','
+#' @param bias, bias method to use : "mirna" , "TF", "CUSTOM", kinasi,immunoSignature, cytoBands, ALL,kegg,reactome
+#' @param bN, name of the custom bias file. This file need header, in the first column has to be the source and in the second column the gene symbol. All path needs to be provided.
+#' @param seed, important value to reproduce the same results with same input
+#' @author Luca Alessandri, alessandri [dot] luca1991 [at] gmail [dot] com, University of Torino
+#'
+#' @examples
+#' \dontrun{
+#' deepClustering(group=c("docker"), scratch.folder="/scratch/", file="/home/lucastormreig/clustering_autoencoder/DCA/Rfunction/setA2.csv",separator=",", nCluster=5, bias="TF", finetune_iters=3, nEpochs=2,seed=1111,projectName="TF",bN="NULL")
+#'}
+#' @export
+deepClustering <- function(group=c("sudo","docker"), scratch.folder, file,separator, nCluster, bias, finetune_iters=100000, nEpochs=50000,seed=1111,projectName,bN="NULL"){
+
+  data.folder=dirname(file)
+positions=length(strsplit(basename(file),"\\.")[[1]])
+matrixNameC=strsplit(basename(file),"\\.")[[1]]
+matrixName=paste(matrixNameC[seq(1,positions-1)],collapse="")
+format=strsplit(basename(basename(file)),"\\.")[[1]][positions]
+
+
+  #running time 1
+  ptm <- proc.time()
+  #setting the data.folder as working folder
+  if (!file.exists(data.folder)){
+    cat(paste("\nIt seems that the ",data.folder, " folder does not exist\n"))
+    return(2)
+  }
+
+  #storing the position of the home folder
+  home <- getwd()
+  setwd(data.folder)
+  #initialize status
+  system("echo 0 > ExitStatusFile 2>&1")
+
+  #testing if docker is running
+  test <- dockerTest()
+  if(!test){
+    cat("\nERROR: Docker seems not to be installed in your system\n")
+    system("echo 10 > ExitStatusFile 2>&1")
+    setwd(home)
+    return(10)
+  }
+
+
+
+  #check  if scratch folder exist
+  if (!file.exists(scratch.folder)){
+    cat(paste("\nIt seems that the ",scratch.folder, " folder does not exist\n"))
+    system("echo 3 > ExitStatusFile 2>&1")
+    setwd(data.folder)
+    return(3)
+  }
+  tmp.folder <- gsub(":","-",gsub(" ","-",date()))
+  scrat_tmp.folder=file.path(scratch.folder, tmp.folder)
+  writeLines(scrat_tmp.folder,paste(data.folder,"/tempFolderID", sep=""))
+  cat("\ncreating a folder in scratch folder\n")
+  dir.create(file.path(scrat_tmp.folder))
+  #preprocess matrix and copying files
+
+if(separator=="\t"){
+separator="tab"
+}
+
+system(paste("cp ",data.folder,"/",matrixName,".",format," ",scrat_tmp.folder,"/",sep=""))
+  if(bias=="CUSTOM"){
+system(paste("cp ",bN," ",scrat_tmp.folder,"/",sep=""))
+}
+  bN=basename(bN)
+  #executing the docker job
+    params <- paste("--cidfile ",data.folder,"/dockerID -v ",scrat_tmp.folder,":/scratch -v ", data.folder, ":/data -d repbioinfo/deepclae_cpu python3 /home/main.py ",matrixNameC,".",format," ",separator," ",nCluster," ",bias," ",finetune_iters," ",nEpochs," ",projectName," ",seed," ",bN,sep="")
+
+resultRun <- runDocker(group=group, params=params)
+
+  #waiting for the end of the container work
+  if(resultRun==0){
+    #system(paste("cp ", scrat_tmp.folder, "/* ", data.folder, sep=""))
+  }
+  #running time 2
+  ptm <- proc.time() - ptm
+  dir <- dir(data.folder)
+  dir <- dir[grep("run.info",dir)]
+  if(length(dir)>0){
+    con <- file("run.info", "r")
+    tmp.run <- readLines(con)
+    close(con)
+    tmp.run[length(tmp.run)+1] <- paste("user run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
+    writeLines(tmp.run,"run.info")
+  }else{
+    tmp.run <- NULL
+    tmp.run[1] <- paste("run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
+
+    writeLines(tmp.run,"run.info")
+  }
+
+  #saving log and removing docker container
+  container.id <- readLines(paste(data.folder,"/dockerID", sep=""), warn = FALSE)
+  system(paste("docker logs ", substr(container.id,1,12), " > ",data.folder,"/", substr(container.id,1,12),".log 2>&1", sep=""))
+  system(paste("docker rm ", container.id, sep=""))
+
+
+  #Copy result folder
+  cat("Copying Result Folder")
+  system(paste("cp -r ",scrat_tmp.folder,"/* ",data.folder,sep=""))
+  #removing temporary folder
+  cat("\n\nRemoving the temporary file ....\n")
+  system(paste("rm -R ",scrat_tmp.folder))
+  system("rm -fR out.info")
+  system("rm -fR dockerID")
+  system("rm  -fR tempFolderID")
+  setwd(home)
+}
